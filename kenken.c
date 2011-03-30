720cee377eb81fc3481b69793dcc3cd17efa52ef
Ben Parr <bparr@cmu.edu>
2011-03-30 00:43:01 -0400

    Merge algorithm functions

--- kenken.c
+++ kenken.c
@@ -3,8 +3,6 @@
  *	@author Jonathan Park (jjp1) and Ben Parr (bparr)
  */
 
-// TODO fix naming inconsistencies
-
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,7 +13,7 @@
 // TODO make POSSIBLE = 0?
 #define POSSIBLE 3 // Value of possible such that it actually is possible
 #define UNASSIGNED_VALUE 0
-#define CONSTRAINT_NUM 3
+#define NUM_CELL_CONSTRAINTS 3
 
 #define MAX_LINE_LEN 2048
 
@@ -53,7 +51,7 @@ typedef struct constraint {
 typedef struct cell {
   int value;
   possible_t possibles;
-  constraint_t* constraints[CONSTRAINT_NUM];
+  constraint_t* constraints[NUM_CELL_CONSTRAINTS];
 } cell_t;
 
 // Funtions to initialize line constraints
@@ -61,7 +59,6 @@ void initRowConstraint(constraint_t* constraint, int row);
 void initColumnConstraint(constraint_t* constraint, int col);
 
 // Functions to initialize constraint possibles
-// TODO: fill in from Ben
 void initLinePossibles(possible_t* possibles);
 void initPlusPossibles(possible_t* possibles, long value, int numCells);
 void initMinusPossibles(possible_t* possibles, long value, int numCells);
@@ -70,13 +67,9 @@ void initDividePossibles(possible_t* possibles, long value, int numCells);
 void initSinglePossibles(possible_t* possibles, long value, int numCells);
 
 // Algorithm functions
-// TODO merge some of these functions?
 int solve(int step);
-int findNextCell();
-void assignValue(cell_t* cell, int value);
-void unassignValue(cell_t* cell, int value);
-void addCell(constraint_t* constraint, cell_t* cell, int value);
-void removeCell(constraint_t* constraint, cell_t* cell, int value);
+void removeCell(constraint_t* constraint, int value);
+void addCell(constraint_t* constraint, int value);
 
 // Miscellaneous functions
 void usage(char* program);
@@ -210,111 +203,67 @@ int main(int argc, char **argv)
 
 // Main recursive function used to solve the program
 int solve(int step) {
-  int i;
+  int i, j, numPossibles;
+  int minIndex = 0, minPossibles = INT_MAX;
   cell_t* cell;
 
   // Success if all cells filled in
   if (step == totalNumCells)
     return 1;
 
-  // Get next cell to fill
-  cell = &(cells[findNextCell()]);
+  // Find the unassigned cell with the mininum number of possibilities
+  for (i = 0; i < totalNumCells; i++) {
+    cell = &(cells[i]);
 
-  // Fail early if next cell has no possibilities
-  if (cell->possibles.num == 0)
-    return 0;
-
-  // Try all possible values
-  for (i = 1; i <= N; i++) {
-    if (cell->possibles.flags[i] != POSSIBLE)
-      continue;
-
-    assignValue(cell, i);
-    if (solve(step + 1))
-      return 1;
-    unassignValue(cell, i);
-  }
-
-  // Fail if none of possibilities worked
-  return 0;
-}
-
-// Find and return index of the next cell to fill. The next cell is the
-// unassigned cell with mininum number of possibilities.
-int findNextCell() {
-  int i, numPossibles;
-  int minIndex = 0, minPossibles = INT_MAX;
-
-  // Loop and check all unassigned cells, ending early if found one with no
-  // possibilities (can't find a cell with fewer possibilities than zero)
-  for (i = 0; minPossibles > 0 && i < totalNumCells; i++) {
     // Skip assigned cells
-    if (cells[i].value != UNASSIGNED_VALUE)
+    if (cell->value != UNASSIGNED_VALUE)
       continue;
 
-    numPossibles = cells[i].possibles.num;
+    numPossibles = cell->possibles.num;
+
+    // Fail early if found unassigned cell with no possibilities
+    if (numPossibles == 0)
+      return 0;
+
     if (numPossibles < minPossibles) {
       minIndex = i;
       minPossibles = numPossibles;
     }
   }
 
-  return minIndex;
-}
+  // Use the found cell as the next cell to fill
+  cell = &(cells[minIndex]);
 
-// Assigns a value to a cell
-void assignValue(cell_t* cell, int value) {
-  int i;
+  // Try all possible values for next cell
+  for (i = 1; i <= N; i++) {
+    if (cell->possibles.flags[i] != POSSIBLE)
+      continue;
 
-  // Assign value
-  cell->value = value;
+    cell->value = i;
 
-  // Remove cell from constraints
-  for (i = 0; i < CONSTRAINT_NUM; i++)
-    removeCell(cell->constraints[i], cell, value);
-}
+    // Remove cell from its constraints
+    for (j = 0; j < NUM_CELL_CONSTRAINTS; j++)
+      removeCell(cell->constraints[i], i);
 
-// Unassigns a value to a cell
-// TODO mark as impossible
-void unassignValue(cell_t* cell, int value) {
-  int i;
+    if (solve(step + 1))
+      return 1;
 
-  // Set value to unassigned
-  cell->value = UNASSIGNED_VALUE;
-
-  // Add cell back to its constraints
-  for (i = 0; i < CONSTRAINT_NUM; i++)
-    addCell(cell->constraints[i], cell, value);
-}
-
-// Adds a cell to a constraint
-void addCell(constraint_t* constraint, cell_t* cell, int value) {
-  constraint->numCells++;
-
-  switch (constraint->type) {
-    case PLUS:
-      constraint->value += value;
-      break;
-    case PARTIAL_MINUS:
-      // TODO: Partial minus converge
-      break;
-    case MULTIPLY:
-      constraint->value *= value;
-      break;
-    case PARTIAL_DIVIDE:
-      // TODO: Partial divide converge
-      break;
-    default:
-      break;
+    // Add cell back to its constraints
+    // TODO optimization: avoid adding cell back just to remove it?
+    for (j = 0; j < NUM_CELL_CONSTRAINTS; j++)
+      addCell(cell->constraints[i], i);
   }
 
-  // TODO: Ben's compute Possibilities
+  // Unassign value and fail if none of possibilities worked
+  cell->value = UNASSIGNED_VALUE;
+  return 0;
 }
 
-// Removes a cell from a constraint
-void removeCell(constraint_t* constraint, cell_t* cell, int value) {
+// Remove a cell with certain value from a constraint
+void removeCell(constraint_t* constraint, int value) {
   constraint->numCells--;
 
+  // TODO implement
   switch (constraint->type) {
     case PLUS:
       constraint->value -= value;
@@ -327,16 +276,33 @@ void removeCell(constraint_t* constraint, cell_t* cell, int value) {
       break;
     case DIVIDE:
       constraint->type = PARTIAL_DIVIDE;
-      // TODO: div case
       break;
     case SINGLE:
       break;
     default:
       break;
   }
+}
 
+// Add a cell with a specific value to a constraint
+void addCell(constraint_t* constraint, int value) {
+  constraint->numCells++;
 
-  // TODO: Ben updates constraint's possible values
+  // TODO implement
+  switch (constraint->type) {
+    case PLUS:
+      constraint->value += value;
+      break;
+    case PARTIAL_MINUS:
+      break;
+    case MULTIPLY:
+      constraint->value *= value;
+      break;
+    case PARTIAL_DIVIDE:
+      break;
+    default:
+      break;
+  }
 }
 
 // Initializes a row constraint for the given row
